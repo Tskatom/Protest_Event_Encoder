a42f0df8aa6abb15db1a94622d1efeb4389b9491
tskatom@vt.edu <tskatom@gmail.com>
2015-12-26 13:30:23 -0500

    generate multi_label dataset

--- util/generate_normal_set.py
+++ util/generate_normal_set.py
@@ -14,10 +14,15 @@ Prepare the normal text data for classifiers
 """
 
 infile = "../data/all_gsr_events_BoT-March_2015.hdl-desc-dwnldarticles.translated.enr.json"
-es_file = "../data/spanish_protest.txt.tok"
-en_file = "../data/english_protest.txt.tok"
-pop_file = "../data/pop_label.txt"
-type_file = "../data/eventType_label.txt"
+es_file = "../data/multi_label/spanish_protest.txt.tok"
+en_file = "../data/multi_label/english_protest.txt.tok"
+pop_file = "../data/multi_label/pop_label.txt"
+type_file = "../data/multi_label/eventType_label.txt"
+
+s_es_file = "../data/single_label/spanish_protest.txt.tok"
+s_en_file = "../data/single_label/english_protest.txt.tok"
+s_pop_file = "../data/single_label/pop_label.txt"
+s_type_file = "../data/single_label/eventType_label.txt"
 
 key_file = "../data/seed_keywords.txt"
 words = [w.strip() for w in open(key_file)]
@@ -25,7 +30,8 @@ rule = '|'.join(words)
 pattern = re.compile(rule, re.I)
 
 
-with open(infile) as itf, open(es_file, 'w') as esf, open(en_file, 'w') as enf, open(pop_file, 'w') as pop_f, open(type_file, 'w') as tf:
+with open(infile) as itf, open(es_file, 'w') as esf, open(en_file, 'w') as enf, open(pop_file, 'w') as pop_f, open(type_file, 'w') as tf, open(s_es_file, 'w') as s_esf, open(s_en_file, 'w') as s_enf, open(s_pop_file, 'w') as s_pop_f, open(s_type_file, 'w') as s_tf:
+    article_set = {}
     for line in itf:
         event = json.loads(line)
         # for each event it may contain multiple articles
@@ -58,14 +64,26 @@ with open(infile) as itf, open(es_file, 'w') as esf, open(en_file, 'w') as enf, 
             # check duplicated
             article_key = ' '.join(spanish_tokens[:100])
             if article_key not in article_set:
-                article_set[article_key] = 1
-                # write out the articles to file
-                esf.write(u' '.join(spanish_tokens).encode('utf-8') + "\n")
-                enf.write(u' '.join(english_tokens).encode('utf-8') + "\n")
+                article_set[article_key] = {"spanish": u' '.join(spanish_tokens).encode('utf-8'), 
+                        "english": u' '.join(english_tokens).encode('utf-8'),
+                        "pop":set(population), "type":set(eventType)}
+            else:
+                article_set[article_key]["pop"].add(population)
+                article_set[article_key]["type"].add(eventType)
+    print "Total Articles %d " % len(article_set)
+    # write to files
+    for article_key, value in article_set.items():
+        esf.write(value["spanish"] + "\n")
+        enf.write(value["english"] + "\n")
+        pop_f.write('|'.join(value['pop']) + "\n")
+        tf.write('|'.join(value['type']) + "\n")
 
-                pop_f.write(population + "\n")
-                tf.write(eventType + "\n")
-
+        # write to single label
+        if len(value['pop']) == 1:
+            s_esf.write(value["spanish"] + "\n")
+            s_enf.write(value["english"] + "\n")
+            s_pop_f.write(value["pop"].pop() + "\n")
+            s_tf.write(value["type"].pop() + "\n")
 
 if __name__ == "__main__":
     pass
