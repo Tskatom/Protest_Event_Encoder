aabc20f24f0884034d22a8e611014049f2859c98
Ubuntu <ubuntu@ip-172-31-31-214.ec2.internal>
2016-01-11 19:38:26 +0000

    update experiments result

--- experiments/DLBE_batch.sh
+++ experiments/DLBE_batch.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-bash dlbe_pop.sh 1
+#bash dlbe_pop.sh 1
 echo "Fnish  bash dlbe_pop_.sh 1"
-bash dlbe_type.sh 1
+#bash dlbe_type.sh 1
 echo "Fnish  bash dlbe_type.sh 1"
-bash DLBE_cnn.sh 1
+#bash DLBE_cnn.sh 1
 echo "Fnish  bash DLBE_cnn.sh 1"
 
-bash dlbe_pop.sh 2
+#bash dlbe_pop.sh 2
 echo "Fnish  bash dlbe_pop_.sh 2"
-bash dlbe_type.sh 2
+#bash dlbe_type.sh 2
 echo "Fnish  bash dlbe_type.sh 2"
-bash DLBE_cnn.sh 2
+#bash DLBE_cnn.sh 2
 echo "Fnish  bash DLBE_cnn.sh 2"
 
-bash dlbe_pop.sh 3
+#bash dlbe_pop.sh 3
 echo "Fnish  bash dlbe_pop.sh 3"
-bash dlbe_type.sh 3
+#bash dlbe_type.sh 3
 echo "Fnish  bash dlbe_type.sh 3"
-bash DLBE_cnn.sh 3
+#bash DLBE_cnn.sh 3
 echo "Fnish  bash DLBE_cnn.sh 3"
 
 bash dlbe_pop.sh 4
