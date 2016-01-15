1927e985a6c878402306b8ae85ac9c86fbb514e6
Ubuntu <ubuntu@ip-172-31-28-111.ec2.internal>
2016-01-15 16:51:46 +0000

    add CNN multilayer

--- experiments/dlbe_type.sh
+++ experiments/dlbe_type.sh
@@ -11,7 +11,8 @@ max_num=100000
 min_word_count=1
 word_dm=200
 k=$1
-
+d=$2
+freq=$3
 echo Generating vocabulary for training data ... \n
 vocab_fn=data/spanish_protest.trn-${max_num}.vocab
 $prep_exe gen_vocab input_fn=./data/tokens.lst vocab_fn=$vocab_fn max_vocab_size=$max_num \
@@ -24,10 +25,10 @@ pretrained_fn=../data/${word_dm}d_vectors.txt
 python $text_tool --task gen_emb --vocab_fn $vocab_fn --vec_random_fn $vec_random_fn --vec_trained_fn $vec_trained_fn --pretrained_fn $pretrained_fn --emb_dm $word_dm
 
 echo Start Training the model
-exp_name=dlbe_type_w${word_dm}_k${k}_d25
+exp_name=dlbe_type_w${word_dm}_k${k}_d${d}
 log_fn=./log/${exp_name}.log
 perf_fn=./results/
-param_fn=./dlbe_type_param.json
-python $model_exe --prefix ../data/single_label/spanish_protest --sufix type_cat --word2vec $vec_trained_fn --dict_fn ../data/type_cat.dic --max_sens 30 --max_words 70 --padding 3 --exp_name $exp_name --max_iter 75 --batch_size 100 --log_fn $log_fn --perf_fn $perf_fn --param_fn $param_fn --top_k $k 
+param_fn=./dlbe_type_param_d${d}.json
+python $model_exe --prefix ../data/single_label/spanish_protest --sufix type_cat --word2vec $vec_trained_fn --dict_fn ../data/type_cat.dic --max_sens 30 --max_words 70 --padding 3 --exp_name $exp_name --max_iter 75 --batch_size 100 --log_fn $log_fn --perf_fn $perf_fn --param_fn $param_fn --top_k $k --print_freq $freq
 
 
