812f57f21dfe69bc25411faf4fc67e51af1a87db
tskatom <tskatom@vt.edu>
2016-03-05 17:11:59 +0000

    update

--- MI_cnn_kmax_mix.py
+++ MI_cnn_kmax_mix.py
@@ -21,8 +21,10 @@ import numpy as np
 def ReLU(x):
     return T.maximum(0.0, x)
 
+
 def Tanh(x):
     return T.tanh(x)
+
 
 def construct_sentence_flag(digit_dataset):
     train_set, test_set = digit_dataset
@@ -42,14 +44,14 @@ def construct_sentence_flag(digit_dataset):
 
     return train_flags, test_flags
 
+
 def construct_dynamic_k(sentence_flags, k_port):
     flag_sum = np.sum(sentence_flags, axis=1)
     flag_port = np.maximum(1, np.floor(flag_sum * k_port))
     flag_mask = np.zeros_like(sentence_flags)
     for i in range(len(flag_mask)):
-        flag_mask[i,-flag_port[i]:] = 1
+        flag_mask[i, -flag_port[i]:] = 1
     return flag_mask
-
 
 
 class GICF(object):
@@ -66,6 +68,7 @@ class GICF(object):
         word_window = self.options["word_window"]
         word_dim = self.options["word_dim"]
         k_max_word = self.options["k_max_word"]
+        k_max_sentence = self.options["k_max_sentence"]
         batch_size = self.options["batch_size"]
         rho = self.options["rho"]
         epsilon = self.options["epsilon"]
@@ -76,17 +79,29 @@ class GICF(object):
         sentence_window = self.options["sentence_window"]
 
         sentence_len = len(dataset[0][0][0][0])
+        sentence_num = len(dataset[0][0][0])
 
         # compute the sentence flags
         train_flags, test_flags = construct_sentence_flag(dataset)
         train_k_value = construct_dynamic_k(train_flags, k_portion)
         test_k_value = construct_dynamic_k(test_flags, k_portion)
-        
-        train_flags = theano.shared(value=np.asarray(train_flags, dtype=theano.config.floatX), borrow=True)
-        test_flags = theano.shared(value=np.asarray(test_flags, dtype=theano.config.floatX), borrow=True)
-        
-        train_k = theano.shared(value=np.asarray(train_k_value, dtype=theano.config.floatX), borrow=True)
-        test_k = theano.shared(value=np.asarray(test_k_value, dtype=theano.config.floatX), borrow=True)
+
+        train_flags = theano.shared(value=np.asarray(
+            train_flags,
+            dtype=theano.config.floatX),
+            borrow=True)
+        test_flags = theano.shared(value=np.asarray(
+            test_flags,
+            dtype=theano.config.floatX),
+            borrow=True)
+
+        train_k = theano.shared(value=np.asarray(
+            train_k_value,
+            dtype=theano.config.floatX),
+            borrow=True)
+        test_k = theano.shared(value=np.asarray(
+            test_k_value,
+            dtype=theano.config.floatX), borrow=True)
 
         # define the parameters
         x = T.tensor3("x")
@@ -95,66 +110,80 @@ class GICF(object):
         sen_k = T.matrix("sen_k")
         rng = np.random.RandomState(1234)
 
-        words = theano.shared(value=np.asarray(word_embedding,
-            dtype=theano.config.floatX),
+        words = theano.shared(value=np.asarray(
+            word_embedding, dtype=theano.config.floatX),
             name="embedding", borrow=True)
         zero_vector_tensor = T.vector()
         zero_vec = np.zeros(word_dim, dtype=theano.config.floatX)
-        set_zero = theano.function([zero_vector_tensor], updates=[(words, T.set_subtensor(words[0,:], zero_vector_tensor))])
+        set_zero = theano.function(
+            [zero_vector_tensor],
+            updates=[(words, T.set_subtensor(words[0, :], zero_vector_tensor))])
 
-        x_emb = words[T.cast(x.flatten(), dtype="int32")].reshape((x.shape[0]*x.shape[1], 1, x.shape[2], words.shape[1]))
+        x_emb = words[T.cast(x.flatten(), dtype="int32")].reshape(
+            (x.shape[0]*x.shape[1], 1, x.shape[2], words.shape[1]))
 
         dropout_x_emb = nn.dropout_from_layer(rng, x_emb, drop_rate_word)
 
         # compute convolution on words layer
         word_filter_shape = (num_maps_word, 1, word_window, word_dim)
         word_pool_size = (sentence_len - word_window + 1, 1)
-        dropout_word_conv = nn.ConvPoolLayer(rng,
-                input=dropout_x_emb,
-                input_shape=None,
-                filter_shape=word_filter_shape,
-                pool_size=word_pool_size,
-                activation=Tanh,
-                k=k_max_word)
+        dropout_word_conv = nn.ConvPoolLayer(
+            rng,
+            input=dropout_x_emb,
+            input_shape=None,
+            filter_shape=word_filter_shape,
+            pool_size=word_pool_size,
+            activation=Tanh,
+            k=k_max_word)
         sent_vec_dim = num_maps_word*k_max_word
-        dropout_sent_vec = dropout_word_conv.output.reshape((x.shape[0], 1, x.shape[1], sent_vec_dim))
-        dropout_sent_vec = nn.dropout_from_layer(rng, dropout_sent_vec, drop_rate_sentence)
+        dropout_sent_vec = dropout_word_conv.output.reshape(
+            (x.shape[0], 1, x.shape[1], sent_vec_dim))
+        dropout_sent_vec = nn.dropout_from_layer(
+            rng,
+            dropout_sent_vec,
+            drop_rate_sentence)
 
-        word_conv = nn.ConvPoolLayer(rng,
-                input=dropout_x_emb*(1 - drop_rate_word),
-                input_shape=None,
-                filter_shape=word_filter_shape,
-                pool_size=word_pool_size,
-                activation=Tanh,
-                k=k_max_word,
-                W=dropout_word_conv.W,
-                b=dropout_word_conv.b)
-        sent_vec = word_conv.output.reshape((x.shape[0], 1, x.shape[1], sent_vec_dim))
+        word_conv = nn.ConvPoolLayer(
+            rng,
+            input=dropout_x_emb*(1 - drop_rate_word),
+            input_shape=None,
+            filter_shape=word_filter_shape,
+            pool_size=word_pool_size,
+            activation=Tanh,
+            k=k_max_word,
+            W=dropout_word_conv.W,
+            b=dropout_word_conv.b)
+        sent_vec = word_conv.output.reshape(
+            (x.shape[0], 1, x.shape[1], sent_vec_dim))
         sent_vec = sent_vec * (1 - drop_rate_sentence)
 
         # construct doc level context information
-        sent_filter_shape = (num_maps_sentence, 1, sentence_window, sent_vec_dim)
+        sent_filter_shape = (num_maps_sentence, 1,
+                             sentence_window, sent_vec_dim)
         sent_pool_size = (sentence_num - sentence_window + 1, 1)
-        dropout_sent_conv = nn.ConvPoolLayer(rng,
-                input=dropout_sent_vec,
-                input_shape=None,
-                filter_shape=sent_filter_shape,
-                pool_size=sent_pool_size,
-                activation=Tanh,
-                k=k_max_sentence)
+        dropout_sent_conv = nn.ConvPoolLayer(
+            rng,
+            input=dropout_sent_vec,
+            input_shape=None,
+            filter_shape=sent_filter_shape,
+            pool_size=sent_pool_size,
+            ctivation=Tanh,
+            k=k_max_sentence)
 
-        sent_conv = nn.ConvPoolLayer(rng,
-                input=sent_vec,
-                input_shape=None,
-                filter_shape=sent_filter_shape,
-                pool_size=sent_pool_size,
-                activation=Tanh,
-                k=k_max_sentence,
-                W=dropout_sent_conv.W,
-                b=dropout_sent_conv.b)
+        sent_conv = nn.ConvPoolLayer(
+            rng,
+            input=sent_vec,
+            input_shape=None,
+            filter_shape=sent_filter_shape,
+            pool_size=sent_pool_size,
+            activation=Tanh,
+            k=k_max_sentence,
+            W=dropout_sent_conv.W,
+            b=dropout_sent_conv.b)
 
         # reshape the sentence vec
-        dropout_sent_vec = dropout_sent_vec.reshape((x.shape[0], x.shape[1], sent_vec_dim))
+        dropout_sent_vec = dropout_sent_vec.reshape(
+            (x.shape[0], x.shape[1], sent_vec_dim))
         sent_vec = sent_vec.reshape((x.shape[0], x.shape[1], sent_vec_dim))
 
         dropout_doc_vec = dropout_sent_conv.output.flatten(2)
@@ -162,9 +191,19 @@ class GICF(object):
         doc_vec_dim = num_maps_sentence * k_max_sentence
 
         # concatenate the doc vec along with the sentence vector
-        con_dropout_sent_vec = T.concatenate([dropout_sent_vec, T.tile(dropout_doc_vec, [1, x.shape[1]]).reshape((x.shape[0], x.shape[1], doc_vec_dim))], axis=2).reshape((x.shape[0]*x.shape[1], sent_vec_dim + doc_vec_dim))
-        
-        con_sent_vec = T.concatenate([sent_vec, T.tile(doc_vec, [1, x.shape[1]]).reshape((x.shape[0], x.shape[1], doc_vec_dim))], axis=2).reshape((x.shape[0]*x.shape[1], sent_vec_dim + doc_vec_dim))
+        con_dropout_sent_vec = T.concatenate(
+            [dropout_sent_vec, T.tile(
+                dropout_doc_vec,
+                [1, x.shape[1]]).reshape(
+                    (x.shape[0], x.shape[1], doc_vec_dim))],
+            axis=2).reshape((x.shape[0]*x.shape[1], sent_vec_dim + doc_vec_dim))
+
+        con_sent_vec = T.concatenate(
+            [sent_vec, T.tile(
+                doc_vec, [1, x.shape[1]]).reshape(
+                    (x.shape[0], x.shape[1], doc_vec_dim))],
+            axis=2).reshape(
+                (x.shape[0]*x.shape[1], sent_vec_dim + doc_vec_dim))
 
         # construct sentence level classifier
         n_in = sent_vec_dim + doc_vec_dim
@@ -174,40 +213,52 @@ class GICF(object):
         sen_b_value = nn.as_floatX(0.0)
         sen_b = theano.shared(value=sen_b_value, borrow=True, name="logis_b")
 
-        drop_sent_prob = T.nnet.sigmoid(T.dot(con_dropout_sent_vec, sen_W) + sen_b)
+        drop_sent_prob = T.nnet.sigmoid(
+            T.dot(con_dropout_sent_vec, sen_W) + sen_b)
         sent_prob = T.nnet.sigmoid(T.dot(con_sent_vec, sen_W) + sen_b)
 
         # reform the sent vec to doc level
         drop_sent_prob = drop_sent_prob.reshape((x.shape[0], x.shape[1]))
         sent_prob = sent_prob.reshape((x.shape[0], x.shape[1]))
 
-        """
-        # the pos probability bag label is the avg of the probs
-        drop_doc_prob = T.sum(drop_sent_prob * sen_flags, axis=1) / T.sum(sen_flags, axis=1)
-        doc_prob = T.sum(sent_prob * sen_flags, axis=1) / T.sum(sen_flags, axis=1)
-        """
         # using the dynamic top k max probability as bag level probability
         # compute the dynamic K for each documents
-        drop_doc_prob = T.sum(T.sort(drop_sent_prob, axis=1) * sen_k, axis=1) / T.sum(sen_k, axis=1)
-        doc_prob = T.sum(T.sort(sent_prob, axis=1) * sen_k, axis=1) / T.sum(sen_k, axis=1)
-        
+        drop_doc_prob = T.sum(
+            T.sort(drop_sent_prob, axis=1) * sen_k,
+            axis=1) / T.sum(sen_k, axis=1)
+        doc_prob = T.sum(
+            T.sort(sent_prob, axis=1) * sen_k,
+            axis=1) / T.sum(sen_k, axis=1)
 
-        drop_doc_prob = T.clip(drop_doc_prob, nn.as_floatX(1e-7), nn.as_floatX(1 - 1e-7 ))
-        doc_prob = T.clip(doc_prob, nn.as_floatX(1e-7), nn.as_floatX(1 - 1e-7 ))
+        drop_doc_prob = T.clip(
+            drop_doc_prob,
+            nn.as_floatX(1e-7),
+            nn.as_floatX(1 - 1e-7))
+        doc_prob = T.clip(
+            doc_prob,
+            nn.as_floatX(1e-7),
+            nn.as_floatX(1 - 1e-7))
 
         doc_preds = doc_prob > 0.5
 
         # instance level cost
-        drop_sent_cost = T.sum(T.maximum(0.0, nn.as_floatX(.5) - T.sgn(drop_sent_prob.reshape((x.shape[0]*x.shape[1], n_out)) - nn.as_floatX(0.6)) * T.dot(dropout_sent_vec, sen_W)) * sen_flags.reshape((x.shape[0]*x.shape[1], n_out))) / T.sum(sen_flags)
+        drop_sent_cost = T.sum(
+            T.maximum(0.0, nn.as_floatX(.5) -
+                      T.sgn(drop_sent_prob.reshape(
+                          (x.shape[0]*x.shape[1], n_out)) -
+                          nn.as_floatX(0.6)) * T.dot(dropout_sent_vec, sen_W)) *
+            sen_flags.reshape(
+                (x.shape[0]*x.shape[1], n_out))) / T.sum(sen_flags)
 
         # we need that the most positive instance at least 0.7 in pos bags
         # and at most 0.1 in neg bags
         # we want the number of positive instance should at least ...
         # and non of the positive instances in the negative bags
-        
+
         # compute the number of positive instance
         positive_count = T.sum((drop_sent_prob * sen_flags) > 0.5, axis=1)
-        pos_cost = T.maximum(nn.as_floatX(0.0), nn.as_floatX(2) - positive_count)
+        pos_cost = T.maximum(nn.as_floatX(0.0),
+                             nn.as_floatX(2) - positive_count)
         neg_cost = T.maximum(nn.as_floatX(0.0), positive_count)
         penal_cost = T.mean(pos_cost * y + neg_cost * (nn.as_floatX(1.0) - y))
 
@@ -216,22 +267,28 @@ class GICF(object):
         sen_sqr = T.sum(dropout_sent_vec ** 2, axis=1)
         sen_sqr_left = sen_sqr.dimshuffle(0, 'x')
         sen_sqr_right = sen_sqr.dimshuffle('x', 0)
-        sen_sim_matrix = sen_sqr_left - 2 * sen_sqr + sen_sqr_right
+        sen_sim_matrix = sen_sqr_left - 2 * sen_sen + sen_sqr_right
         sen_sim_matrix = T.exp(-1 * sen_sim_matrix)
 
-        sen_sim_prob = drop_sent_prob.reshape((x.shape[0]*x.shape[1], 1)) - drop_sent_prob.flatten()
+        sen_sim_prob = drop_sent_prob.reshape(
+            (x.shape[0]*x.shape[1], 1)) - drop_sent_prob.flatten()
         sen_sim_prob = sen_sim_prob ** 2
 
-        sen_sim_flag = T.dot(sen_flags.reshape((x.shape[0]*x.shape[1],1)), sen_flags.reshape((1,x.shape[0]*x.shape[1])))
+        sen_sim_flag = T.dot(
+            sen_flags.reshape((x.shape[0] * x.shape[1], 1)),
+            sen_flags.reshape((1, x.shape[0] * x.shape[1])))
 
-        sen_sim_cost = T.sum(sen_sim_matrix * sen_sim_prob * sen_sim_flag) / T.sum(sen_sim_flag)
-
+        sen_sim_cost = T.sum(
+            sen_sim_matrix * sen_sim_prob * sen_sim_flag) / T.sum(sen_sim_flag)
 
         # bag level cost
-        drop_bag_cost = T.mean(-y * T.log(drop_doc_prob) * nn.as_floatX(0.6) - (1 - y) * T.log(1 - drop_doc_prob) * nn.as_floatX(0.4))
-        #drop_cost = drop_bag_cost * nn.as_floatX(3.0) + drop_sent_cost + nn.as_floatX(2.0) * penal_cost
-        drop_cost = drop_bag_cost * nn.as_floatX(0.6) + drop_sent_cost * nn.as_floatX(0.1) + penal_cost * nn.as_floatX(0.5)  + sen_sim_cost * nn.as_floatX(0.0001)
-
+        drop_bag_cost = T.mean(-y * T.log(drop_doc_prob) * nn.as_floatX(0.6) -
+                               (1 - y) *
+                               T.log(1 - drop_doc_prob) * nn.as_floatX(0.4))
+        drop_cost = drop_bag_cost * nn.as_floatX(0.6) + \
+            drop_sent_cost * nn.as_floatX(0.1) + \
+            penal_cost * nn.as_floatX(0.5) + \
+            sen_sim_cost * nn.as_floatX(0.0001)
 
         # collect parameters
         self.params.append(words)
@@ -240,11 +297,12 @@ class GICF(object):
         self.params.append(sen_W)
         self.params.append(sen_b)
 
-        grad_updates = nn.sgd_updates_adadelta(self.params,
-                drop_cost,
-                rho,
-                epsilon,
-                norm_lim)
+        grad_updates = nn.sgd_updates_adadelta(
+            self.params,
+            drop_cost,
+            rho,
+            epsilon,
+            norm_lim)
 
         # construct the dataset
         # random the
@@ -257,33 +315,45 @@ class GICF(object):
 
         # construt the model
         index = T.iscalar()
-        train_func = theano.function([index], [drop_cost, drop_bag_cost, drop_sent_cost, penal_cost, sen_sim_cost], updates=grad_updates,
-                givens={
-                    x: train_x[index*batch_size:(index+1)*batch_size],
-                    y: train_y[index*batch_size:(index+1)*batch_size],
-                    sen_flags: train_flags[index*batch_size:(index+1)*batch_size],
-                    sen_k: train_k[index*batch_size:(index+1)*batch_size]
-                    })
+        train_func = theano.function(
+            [index],
+            [drop_cost,
+             drop_bag_cost,
+             drop_sent_cost,
+             penal_cost,
+             sen_sim_cost],
+            updates=grad_updates,
+            givens={
+                x: train_x[index*batch_size:(index+1)*batch_size],
+                y: train_y[index*batch_size:(index+1)*batch_size],
+                sen_flags: train_flags[index*batch_size:(index+1)*batch_size],
+                sen_k: train_k[index*batch_size:(index+1)*batch_size]
+            })
 
-        test_func = theano.function([index], doc_preds,
-                givens={
-                    x:test_x[index*batch_size:(index+1)*batch_size],
-                    sen_k: test_k[index*batch_size:(index+1)*batch_size]
-                    })
+        test_func = theano.function(
+            [index],
+            doc_preds,
+            givens={
+                x: test_x[index*batch_size:(index+1)*batch_size],
+                sen_k: test_k[index*batch_size:(index+1)*batch_size]
+            })
 
-        get_train_sent_prob = theano.function([index], sent_prob,
-                givens={
-                    x:train_x[index*batch_size:(index+1)*batch_size]
-                    })
+        get_train_sent_prob = theano.function(
+            [index],
+            sent_prob,
+            givens={
+                x: train_x[index*batch_size:(index+1)*batch_size]
+            })
 
-        get_test_sent_prob = theano.function([index], sent_prob,
-                givens={
-                    x:test_x[index*batch_size:(index+1)*batch_size]
-                    })
+        get_test_sent_prob = theano.function(
+            [index],
+            sent_prob,
+            givens={
+                x: test_x[index*batch_size:(index+1)*batch_size]
+            })
 
         epoch = 0
         best_score = 0
-
 
         log_file = open("./log/%s.log" % exp_name, 'w')
 
@@ -292,8 +362,8 @@ class GICF(object):
             epoch += 1
             costs = []
 
-            for minibatch_index in np.random.permutation(range(n_train_batches)):
-                cost_epoch = train_func(minibatch_index)
+            for mini_index in np.random.permutation(range(n_train_batches)):
+                cost_epoch = train_func(mini_index)
                 costs.append(cost_epoch)
                 set_zero(zero_vec)
 
